7539aa9900851a348855c70b08c7632ca48decee
Geoffrey Martin <geoffreyhershmartin@gmail.com>
2017-06-20 12:52:27 -0400

    new edits

--- 16. Safe Sex Histograms/safesex.py
+++ 16. Safe Sex Histograms/safesex.py
@@ -29,16 +29,17 @@ for i in range(0, len(drinking.columns), 5): # For-loop for creating subplots gr
         if (i + j < 17):
             ax = plt.subplot2grid((4,5), (i/4,j)) # Create subplot
             ax.tick_params(length=0) # Remove tick marks
-            plt.hist(drinking.iloc[2:, i + j].dropna(), bins=range(0,25,1), color = '#b9c9b9')
+            plt.hist(drinking.iloc[2:, i + j].dropna(), bins=range(25), color = '#b9c9b9', normed = True)
             poisson = []
             alpha, beta = list(drinking.iloc[0:2, i + j].dropna()) # Get alpha and beta values from table
             for x in range(25): # Create poisson distrubtion
                 term1 = math.gamma(x+alpha)/math.gamma(alpha)/math.factorial(x)
                 term2 = math.pow(beta, x)/math.pow(1+beta, alpha + x)
                 poisson.append(term1 * term2)
-            poisson = np.asarray(poisson) * len(drinking) # Get expected values for poisson distribution
-            plt.plot(range(25),poisson, color = 'red', ls = 'steps') # Plot poisson distribution
-            plt.xlim(0,25)
+            poisson = [0] + poisson # Append extra data point to shift overlay to the right
+            plt.plot(range(26), poisson, color = 'red', ls = 'steps') # Plot poisson distribution
+            plt.xlim(0, 25)
+            plt.ylim(0,1)
             plt.xticks(fontsize = 7)
             plt.yticks(fontsize = 5)
             plt.title(list(drinking)[i + j], size = 8)
@@ -55,17 +56,18 @@ for i in range(0, len(sexual.columns), 6):
         if (i + j < 14):
             ax = plt.subplot2grid((3,6), (i/6,j))
             ax.tick_params(length=0)
-            plt.hist(sexual.iloc[2:, i + j].dropna(), bins=range(0,25,1), color = '#b9c9b9')
+            plt.hist(sexual.iloc[2:, i + j].dropna(), bins=range(0,35,1), color = '#b9c9b9', normed = True)
             poisson = []
             alpha, beta = list(sexual.iloc[0:2, i + j].dropna())
-            for x in range(25):
+            for x in range(35):
                 term1 = math.gamma(x+alpha)/math.gamma(alpha)/math.factorial(x)
                 term2 = math.pow(beta, x)/math.pow(1+beta, alpha + x)
                 poisson.append(term1 * term2)
-            poisson = np.asarray(poisson) * len(sexual)
-            plt.plot(range(25),poisson, color = 'red', ls = 'steps')
-            plt.xlim(0,25)
-            plt.xticks(fontsize = 7)
+            poisson = [0] + poisson
+            plt.plot(range(36),poisson, color = 'red', ls = 'steps')
+            plt.xlim(0,35)
+            plt.ylim(0,1)
+            plt.xticks(fontsize = 5)
             plt.yticks(fontsize = 5)
             plt.title(list(sexual)[i + j], size = 8)
 plt.tight_layout()
