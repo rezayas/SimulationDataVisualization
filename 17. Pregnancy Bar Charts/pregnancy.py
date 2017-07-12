6b321aeef8139b3f97e940e8c58e031310ba5264
Geoffrey Martin <geoffreyhershmartin@gmail.com>
2017-07-12 00:04:35 -0400

    error bar changes

--- 17. Pregnancy Bar Charts/pregnancy.py
+++ 17. Pregnancy Bar Charts/pregnancy.py
@@ -18,6 +18,7 @@ outcomes = outcomes[1:] # Delete extra column heads
 unintended = outcomes.iloc[0:10,9:19].dropna() # Extract the unintended pregnancies dataset
 projection_period = outcomes.iloc[0:10,30:38] # Extract the projection_period ataset
 projection_period = projection_period * 100 # Convert raw numbers to percentages
+projection_period
 
 # In[1]:
 # Plot the unintended pregnancies ===============================================================
@@ -39,18 +40,21 @@ plt.savefig('unintended_pregnancies.pdf', bbox_inches='tight')
 
 # In[2]:
 # Plot the projection periods (code is modified from above)===============================================================
-plt.bar(range(1,7), projection_period.iloc[0, 1:7], align = 'center', color = 'orange')
-plt.errorbar(range(1,7), projection_period.iloc[0, 1:7], np.array([projection_period.iloc[3, 1:7],
-                projection_period.iloc[4, 1:7]]), fmt = 'none', ecolor = 'red') # Plot CDC error bar
-plt.errorbar(range(1,7), projection_period.iloc[5, 1:7], np.array([projection_period.iloc[8, 1:7],
-                projection_period.iloc[9, 1:7]]), color = 'blue', fmt = 'o') # Plot the other error bar
+fig, ax = plt.subplots()
+ax.bar(range(1,7), projection_period.iloc[0, 1:7], 0.35, color='orange',
+       yerr=np.array([projection_period.iloc[3, 1:7],projection_period.iloc[4, 1:7]]),
+       error_kw=dict(ecolor='red'))
+ax.bar(np.array(range(1,7)) + 0.35, projection_period.iloc[5, 1:7], 0.35, color='#d5deec',
+       yerr=np.array([projection_period.iloc[8, 1:7], projection_period.iloc[9, 1:7]]),
+       error_kw=dict(ecolor='blue'))
 plt.xticks(range(1,7), list(projection_period.columns.values)[1:7])
-plt.xlim([0, 7])
-plt.ylim([0, 20])
-circle = mlines.Line2D([],[], marker='o', markerfacecolor="blue", label = 'Model - Median')
+ax.set_xlim(0.5, 7.2)
+ax.set_ylim(0,20)
+ax.set_xticks(np.array(range(1,7)) + 0.35)
+median = mpatches.Patch(color='#d5deec', label = 'Model - Median')
 cdc = mpatches.Patch(color='orange', label='CDC')
-plt.legend(handles = [cdc, circle], numpoints = 1,
+plt.legend(handles = [cdc, median], numpoints = 1,
            prop={'size':10}, bbox_to_anchor=(1, 1), loc = 2)
 plt.title('% Pregnant at the end of projection period')
 plt.tight_layout()
-plt.savefig('projection_period.pdf', bbox_inches='tight')
+plt.savefig('percentage_pregnant.pdf', bbox_inches='tight')
