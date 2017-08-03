47759cc96f4e3daf570a20d5ef21090d9083b08c
Geoffrey Martin <geoffreyhershmartin@gmail.com>
2017-08-03 01:52:06 -0400

    final changes

--- 23. Agent-Based SIR/agent_based.py
+++ 23. Agent-Based SIR/agent_based.py
@@ -27,34 +27,54 @@ R = pd.ExcelFile('R - model.xlsx')
 R = R.parse('Sheet1')
 S = pd.ExcelFile('S - model.xlsx')
 S = S.parse('Sheet1')
+# Take out the relevant age groups
+model_distribution = model_distribution[0:5]
+data_distribution = data_distribution[0:5]
 
 # In[3]:
 
+# Plot the graphs ===============================================================
 mpl.style.use('classic') # Use classic MPL layout
 fig = plt.figure() # add plot figure
-# add sublots with 2 columns and 2 rows
-fig.add_subplot(231)
-plt.plot(R['Time'], R['Replicate 1'])
-plt.plot(R['Time'], R['Replicate 2'])
+fig.add_subplot(233)
+# Plot all columns for R
+for i in range(1, len(R.columns)):
+    plt.plot(R['Time'], R.iloc[:, i], color = '#787878')
+plt.xlabel('Time')
 plt.title('R')
 fig.add_subplot(232)
-plt.plot(I['Time'], R['Replicate 1'])
-plt.plot(I['Time'], R['Replicate 2'])
+# Plot all columns for I
+for i in range(1, len(I.columns)):
+    plt.plot(I['Time'], I.iloc[:, i], color = '#787878')
+plt.xlabel('Time')
 plt.title('I')
-fig.add_subplot(233)
-plt.plot(S['Time'], R['Replicate 1'])
-plt.plot(S['Time'], R['Replicate 2'])
+fig.add_subplot(231)
+# Plot all columns for S
+for i in range(1, len(S.columns)):
+    plt.plot(S['Time'], S.iloc[:, i], color = '#787878')
+plt.xlabel('Time')
 plt.title('S')
 fig.add_subplot(234)
-plt.plot(model_weekly['Time'], model_weekly['Replicate 1'])
-plt.plot(model_weekly['Time'], model_weekly['Replicate 2'])
+# Plot all columns for weekly data and overlay model
+for i in range(1, len(model_weekly.columns)):
+    plt.plot(model_weekly['Time'], model_weekly.iloc[:, i], color = '#787878')
+for i in range(1, len(data_weekly.columns)):
+    plt.plot(data_weekly['Time'], data_weekly.iloc[:, i], color = 'black',
+             linewidth = '2', marker = 'o', markersize = '5')
+plt.xlabel('Time')
 plt.title('Model')
 ax = fig.add_subplot(235)
-x_axis = np.arange(0, 20) # create y-axis coordinates for scatter plot (dataset) points
+x_axis = np.arange(1, 6) # create y-axis coordinates for scatter plot (dataset) points
+# Plot all columns for age distribution data
 for i in range(0, len(model_distribution)):
-    plt.scatter(x_axis[i], model_distribution['Replicate 1'][i], marker = '_', color = '#C74EFF') # plot a scatter plot with dataset values
-    plt.scatter(x_axis[i], model_distribution['Replicate 2'][i], marker = '_', color = '#C74EFF') # plot a scatter plot with dataset values
-ax.set_xticklabels(model_distribution['Age'],  fontsize = 5)
-y_labels = ax.get_yticklabels()
-ax.set_yticklabels(y_labels, fontsize = 5)
+    for j in range(1, len(model_distribution.columns)):
+        plt.scatter(x_axis[i], model_distribution.iloc[:, j][i], marker = '_', color = '#C74EFF', s = 200) # plot a scatter plot with dataset values
+        plt.scatter(x_axis[i], data_distribution['%'][i], marker = '_', color = '#C74EFF', s = 200) # plot a scatter plot with dataset values
+ax.set_xticks(x_axis)
+ax.set_xticklabels(model_distribution['Age'],  fontsize = 6)
+plt.yticks(fontsize = 6)
+plt.xlabel('Age Group')
+plt.ylabel('Age Distribution')
+plt.title('Distribution')
+plt.tight_layout()
 plt.savefig('Agent-BASED.pdf')
